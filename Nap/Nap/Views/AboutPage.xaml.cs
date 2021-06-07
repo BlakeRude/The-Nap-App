d97579f6fef444cae5c17c8aa3315a6c97f5faf5
BlakeRude <rude3924@vandals.uidaho.edu>
2021-06-07 04:06:36 -0700

    Initial Commit

---
+++ Nap/Nap/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nap.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Nap/Nap/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Nap.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Nap.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
