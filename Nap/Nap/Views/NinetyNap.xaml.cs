bca60a13b170c76d79c6c4c0ca8113d4797ee2ab
BlakeRude <rude3924@vandals.uidaho.edu>
2021-06-09 22:38:14 -0700

    Page Navigation Added
    Added page navigation to the 3 napping screens, as well as the settings page

---
+++ Nap/Nap/Views/NinetyNap.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nap.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NinetyNap : ContentPage
+    {
+        public NinetyNap()
+        {
+            InitializeComponent();
+        }
+    }
+}
